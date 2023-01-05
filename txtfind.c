b10796626a91915589d656a35cd40107b6efd15d
2shlomi9 <118685589+2shlomi9@users.noreply.github.com>
2023-01-05 07:46:45 -0500

    Update txtfind.c

--- txtfind.c
+++ txtfind.c
@@ -78,7 +78,7 @@ int main(){
         }
     }
     int lines=0;
-    while( lines<MAXLINES && scanf("%c",&ch) && ch != EOF){
+    while( lines<MAXLINES && scanf("%c",&ch)){
         i=0;
         char line [LINE]="";
         while(ch != '\n')
@@ -91,7 +91,7 @@ int main(){
             break;
         }
         applyLine(line,str,option);
-        if(++lines>MAXLINES){
+        if(ch == EOF)|| ++lines>MAXLINES){
             break;
         }
     }
