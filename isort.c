0ff2b828e809fb64e882665d7153f50a55718664
2shlomi9 <118685589+2shlomi9@users.noreply.github.com>
2023-01-07 19:28:10 +0200

    add commands

--- isort.c
+++ isort.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define LEN 50
+#define LEN 50 
+/*Declaration of the functions*/
 int shift_element(int* arr, int i);
 int insertion_sort(int* arr, int len);
+
 int main() {
     int size = 0;
     int capacity = 10;
@@ -14,7 +16,7 @@ int main() {
         exit(1);
     }
 
-    while (scanf("%d", &numbers[size]) == 1 && ++size < LEN) {
+    while (scanf("%d", &numbers[size]) == 1 && ++size < LEN) { // scan 50 numbers to the array
         if (size == capacity) {
             capacity *= 2;
             numbers = realloc(numbers, capacity * sizeof(int));
@@ -35,7 +37,14 @@ int main() {
     free(numbers);
     return 0;
 }
-int shift_element(int *arr, int i) {
+/*
+shift_element function:
+this function gets pointer to array of integers, and integer number i.
+the function move the next i organs one step to the right
+-for loop, arr[j] equal to arr[j-1](move one step right)
+-arr[0] equal to n 
+*/  
+int shift_element(int *arr, int i) { 
     int n = *(arr + i);
     for (int j = i; j > 0; j--) {
         *(arr + j) = *(arr + j - 1);
@@ -44,7 +53,16 @@ int shift_element(int *arr, int i) {
     return 0;
 }
 
-
+/*
+Insertion_sort function:
+-two for loop:
+-the first for loop run from i=1 -> array size
+-the second for loop run from j=i-1 -> 0
+-new pointer: *ptr=arr+j 
+-check if ptr>*(arr+i) : 
+(count ++) && (ptr --) ...if j=0 -> move from ptr+1 size of count numbers(use shife_element function) , break
+-else, move from ptr+1 size of count numbers(use shife_element function) , break 
+*/ 
 int insertion_sort(int* arr, int len){
     int count = 0;
     for(int i=1; i<len; i++){
--- txtfind.c
+++ txtfind.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
-#define WORD 30
-#define LINE 256
-#define MAXLINES 250
-#define OPTION1 'a'
-#define OPTION2 'b'
+#define WORD 30 // The max size of a word
+#define LINE 256 // The max size of a line
+#define MAXLINES 250 // Max lines in the text
+#define OPTION1 'a' // print lines
+#define OPTION2 'b' // print word
 
 char Line[LINE] = "";
 char Word[WORD] = "";
 char str[WORD] = "";
 char option;
 
+/*Declaration of the functions*/
+ 
 int get_line();
 
 int getword(char word[]);
@@ -36,6 +38,14 @@ int main() {
     return 0;
 }
 
+/* 
+get_line function :
+* get line from the text :
+-for loop i to the max size of a line
+-use scanf to scan the text into Line
+-if the pointer (Line+i) = end of the file -> *(Line+i) = null , break
+-if the pointer (Line+i) = '\n' -> *(Line+i) = null , break
+*/ 
 int get_line(){
     int i = 0;
     for(i=0;i<LINE;i++){
@@ -51,6 +61,14 @@ int get_line(){
     return i;
 }
 
+/* 
+getword function :
+* gets word from the text :
+-while loop i to the max size of a word
+-use scanf to scan the text into word[]
+-if the pointer (word+i) = end of the file || ' ' || '\t' || '\n' -> *(word+i) = null , break
+-if the pointer (Line+i) = '\n' -> *(Line+i) = null , break
+*/ 
 int getword(char word[]) {
     int i = 0;
     while (i < WORD){
@@ -65,6 +83,16 @@ int getword(char word[]) {
     return i;
 }
 
+/* 
+substring function :
+*check if the str2 contained in str1
+-two for loops :
+-in the first for loop, runs over all str1 chars (i) 
+-in the second for loop,j:0->size of (str2)-1 ,check if *(str1+i+j)=*(str2+j) 
+-if *(str1+i+j)=(str2+j) -> tempSize = tempSize+1
+-in the end of the loops check if tempSize = size of str2(size of our word)
+-if tempSize = size of our word....return true, else false
+*/ 
 int substring(char *str1, char *str2){
     int flag = 0;
     int tempSize = 0;
@@ -90,6 +118,15 @@ int substring(char *str1, char *str2){
     }
     return flag;
 }
+/* 
+similar function :
+* the function will check if it is possible to get string t from string s by deleting n chars  
+-check if size of t + n equal to size of s 
+-while loop runs over all string s (i : 0-> size of s) && lenres: 0-> size of t 
+-if string s in index i equal to string t in index lenres -> lenres++
+-i++
+-if size of lenres equal to size of string t, return true 
+*/ 
 int similar(char *s, char *t, int n) {
     int lens = strlen(s), lent = strlen(t), lenres = 0, i = 0;
     if (lens != lent + n) {
@@ -103,7 +140,13 @@ int similar(char *s, char *t, int n) {
     return (lent == lenres);
 }
 
-
+/* 
+print_lines function :
+* gets lines from the text and prints the lines that contained our word
+-while loop to gets all the lines from the text(use get_lines function)
+-for each line, check if our word contained in this line(use substring function)
+-if our word contained in this line ->print this line 
+*/ 
 void print_lines(){
     while(get_line()){
         if(substring(Line,str)){
@@ -111,15 +154,25 @@ void print_lines(){
         }
     }
 }
-
-
+/* 
+print_similar_words:
+* gets word from the text and prints the word that similar to our word
+-while loop to gets all the words from the text(use getword function)
+-for each word, check if our word similar to this word by deleting one char(use similar function)
+-if our word similar to this word -> print this word
+*/
 void print_similar_words() {
     while (getword(Word)) {
         if (similar(Word, str, 0) || similar(Word, str, 1))
             printf("%s\n", Word);
     }
 }
-
+/*
+run_options function:
+* gets input from the user
+-OPTION1 print_lines
+-OPTION2 print_similar_word
+*/
 int run_options() {
     switch (option) {
         case OPTION1:
